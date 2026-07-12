1ef04592ad0ce457a82ffa43129db9e3d92573e0
ksvarma06 <krishnasaivarma06@gmail.com>
2026-07-12 18:43:08 +0530

    Add node polyfills for crypto and stream to fix white screen in prod

--- AgriTrust/frontend/vite.config.ts
+++ AgriTrust/frontend/vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // @ts-ignore
 import wasm from 'vite-plugin-wasm'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/IndiaCodex-2026/',
   plugins: [
     react(),
-    wasm()
+    wasm(),
+    nodePolyfills()
   ],
   define: {
     global: 'globalThis',
