f55726a1e285db22719e09395bd69c41f1ede4fa
ksvarma06 <krishnasaivarma06@gmail.com>
2026-07-12 18:39:45 +0530

    Use native ESNext target for top-level await

--- AgriTrust/frontend/vite.config.ts
+++ AgriTrust/frontend/vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore
 import wasm from 'vite-plugin-wasm'
-import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/IndiaCodex-2026/',
   plugins: [
     react(),
-    wasm(),
-    topLevelAwait()
+    wasm()
   ],
   define: {
     global: 'globalThis',
   },
+  build: {
+    target: 'esnext'
+  }
 })
